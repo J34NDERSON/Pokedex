c232a73026f35449081ffdd7a63a994b727b6b56
jeanderson wylis <jeanjeandersonwylisdossantos@gmail.com>
2024-03-21 16:53:06 -0300

    add PokemonCards

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,34 @@
+import { Link } from "react-router-dom";
 import { useQueryPokemonPage } from "../../hooks/useQueryPokemonPages";
 import { Container } from "./style";
+import { PokemonCard } from "../../components/pokemonCards";
 
 export function Home() {
-  const { data } = useQueryPokemonPage();
+  const { data, isLoading, error, page, totalPages } = useQueryPokemonPage();
+  console.log(data);
   return (
     <Container>
-      <h1>Home</h1>
-      <p>{data}</p>
+      <h1>{"Bem vindo(a) à pokédex do reprograma jucás"}</h1>
+      {isLoading && <samp className="loading">Loading...</samp>}
+      {isLoading && error && <samp className="loading">Error...</samp>}
+
+      <div className="gridCards">
+        {data?.map((pokemon) => {
+          return (
+            <Link to={"/details"} key={pokemon.id}>
+              <PokemonCard pokemon={pokemon} />
+            </Link>
+          );
+        })}
+      </div>
+
+      <div className="paginationComponent">
+        <button>&alt; Anterior</button>
+        <span className="numberPage">
+          {page}/{totalPages}
+        </span>
+        <button>Próxima &get;</button>
+      </div>
     </Container>
   );
 }
